0611fdfec30f5fc4e0d442e6aa597a1127ae1f9d
HinaOhsawa <strobe.hina@gmail.com>
2025-07-03 23:27:06 +0900

    spotifyAPIを試す

---
+++ src/lib/spotify.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+class SpotifyClient {
+  static async initialize() {
+    const response = await axios.post("https://accounts.spotify.com/api/token", {
+      grant_type: "client_credentials",
+      client_id: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
+      client_secret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET,
+    }, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+
+    let spotify = new SpotifyClient();
+    spotify.token = response.data.access_token;
+    return spotify;
+  }
+
+  async getPopularSongs() {
+    const response = await axios.get(
+      "https://api.spotify.com/v1/playlists/5SLPaOxQyJ8Ne9zpmTOvSe",
+      {
+        headers: { Authorization: `Bearer ${this.token}` },
+      }
+    )
+    console.log(response.data);
+  }
+}
+
+const spotify = await SpotifyClient.initialize();
+export default spotify;
+
+export const getToken = async () => {
+  const response = await axios.post("https://accounts.spotify.com/api/token", {
+    grant_type: "client_credentials",
+    client_id: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
+    client_secret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET,
+  }, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  })
+};
